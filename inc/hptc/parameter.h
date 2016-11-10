3dbda2f2de2323f76810e883922d6eb21bc7f193
Tong Su <tongsu.cn@gmail.com>
2016-11-10 21:38:19 +0100

    Add parameter header file.

---
+++ inc/hptc/parameter.h
@@ -0,0 +1,19 @@
+#pragma once
+#ifndef HPTC_PARAMETER_H_
+#define HPTC_PARAMETER_H_
+
+#include <hptc/types.h>
+
+namespace hptc {
+
+struct Param {
+};
+
+struct ParamTrans : public Param {
+  ;
+};
+
+}
+
+
+#endif // HPTC_PARAMETER_H_
