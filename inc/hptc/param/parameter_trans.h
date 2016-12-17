3d82c796d60dbaaf674075960ceeba307b2a0732
Tong Su <tongsu.cn@gmail.com>
2016-12-17 15:00:45 +0100

    Update transpose parameter.

--- inc/hptc/param/parameter_trans.h
+++ inc/hptc/param/parameter_trans.h
@@ -3,7 +3,7 @@
 #define HPTC_PARAM_PARAMETER_TRANS_H_
 
 #include <memory>
-#include <vector>
+#include <array>
 #include <algorithm>
 
 #include <hptc/types.h>
@@ -13,46 +13,42 @@
 namespace hptc {
 
 template <typename FloatType,
+          TensorOrder ORDER,
           MemLayout LAYOUT = COL_MAJOR>
 struct ParamTrans {
-  using Deduced = DeducedFloatType<FloatType>;
-
-  ParamTrans(const TensorWrapper<FloatType, LAYOUT> &input_tensor,
+  ParamTrans(const TensorWrapper<FloatType, ORDER, LAYOUT> &input_tensor,
       const TensorWrapper<FloatType, LAYOUT> &output_tensor,
-      const std::vector<TensorDim> &perm,
-      Deduced alpha = 1.0, Deduced beta = 0.0);
+      const std::array<TensorOrder, ORDER> &perm,
+      DeducedFloatType<FloatType> alpha = 1.0,
+      DeducedFloatType<FloatType> beta = 0.0);
 
   ParamTrans(const ParamTrans &param) = delete;
   ParamTrans &operator=(const ParamTrans &param) = delete;
 
-  ~ParamTrans();
-
-
-  TensorWrapper<FloatType, LAYOUT> input_tensor, output_tensor;
-  TensorDim *perm;
-  Deduced alpha, beta;
+  TensorWrapper<FloatType, ORDER, LAYOUT> input_tensor, output_tensor;
+  TensorOrder perm[ORDER];
+  DeducedFloatType<FloatType> alpha, beta;
 
   TensorIdx input_stride, output_stride;
-  TensorIdx *macro_loop_idx, **macro_loop_perm_idx;
-
-private:
-  const TensorDim tensor_dim_;
+  TensorIdx macro_loop_idx[ORDER];
+  TensorIdx *macro_loop_perm_idx[ORDER];
 };
 
 
 /*
  * Implementation for class ParamTrans
  */
-template <typename FloatType>
-ParamTrans<FloatType>::ParamTrans(
+template <typename FloatType,
+          TensorOrder ORDER,
+          MemLayout LAYOUT = COL_MAJOR>
+ParamTrans<FloatType, ORDER, LAYOUT>::ParamTrans(
     const TensorWrapper<FloatType, LAYOUT> &input_tensor,
     const TensorWrapper<FloatType, LAYOUT> &output_tensor,
-    const std::vector<TensorDim> &perm, Deduced alpha, Deduced beta)
+    const std::array<TensorOrder, ORDER> &perm,
+    DeducedFloatType<FloatType> alpha, DeducedFloatType<FloatType> beta)
     : input_tensor(input_tensor), output_tensor(output_tensor),
-      perm(new TensorDim [perm.size()]),
       alpha(alpha), beta(beta),
-      input_stride(1), output_stride(1),
-      tensor_dim_(input_tensor.get_dim()) {
+      input_stride(1), output_stride(1) {
   // Initialize permutation map
   copy(perm.begin(), perm.end(), this->perm);
 
@@ -64,26 +60,15 @@ ParamTrans<FloatType>::ParamTrans(
       this->output_stride *= output_tensor.get_outer_size()[idx];
   }
   else {
-    for (TensorIdx idx = this->tensor_dim_ - 1;
-        idx > this->perm[this->tensor_dim_ - 1]; --idx)
+    for (TensorIdx idx = ORDER - 1; idx > this->perm[ORDER - 1]; --idx)
       this->input_stride *= input_tensor.get_outer_size()[idx];
-    for (TensorIdx idx = this->tensor_dim_ - 1;
-        this->tensor_dim_ - 1 != this->perm[idx]; --idx)
+    for (TensorIdx idx = ORDER - 1; ORDER - 1 != this->perm[idx]; --idx)
       this->output_stride *= output_stride.get_outer_size()[idx];
   }
 
   // Initialize loop indices
-  this->macro_loop_idx = new TensorIdx [this->tensor_dim_];
-  this->macro_loop_perm_idx = new TensorIdx * [this->tensor_dim_];
-  for (TensorDim idx = 0; idx < this->tensor_dim_; ++idx)
+  for (TensorOrder idx = 0; idx < ORDER; ++idx)
     this->macro_loop_perm_idx[idx] = &this->macro_loop_idx[this->perm[idx]];
-}
-
-
-template <typename FloatType>
-ParamTrans<FloatType>::~ParamTrans() {
-  delete [] this->perm;
-  delete [] this->macro_loop_idx;
 }
 
 }
