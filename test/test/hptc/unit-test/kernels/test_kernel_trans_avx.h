aefca87523fc74e9dad91d6921afb26ca8c89197
Tong Su <tongsu.cn@gmail.com>
2017-01-07 23:58:27 +0100

    Update previous tests and add transpose macro kernel test.

--- test/test/hptc/unit-test/kernels/test_kernel_trans_avx.h
+++ test/test/hptc/unit-test/kernels/test_kernel_trans_avx.h
@@ -70,17 +70,17 @@ protected:
   }
 
   template <CoefUsage USAGE>
-  void calc_ref(TensorOrder offset_org_0, TensorOrder offset_org_1,
-      TensorOrder offset_ref_0, TensorOrder offset_ref_1, GenNumType width) {
+  void calc_ref(TensorIdx offset_org_0, TensorIdx offset_org_1,
+      TensorIdx offset_ref_0, TensorIdx offset_ref_1, GenNumType width) {
     // Reset reference data
     copy(this->org_data, this->org_data + this->data_len, this->ref_data);
 
     // Compute reference
-    for (TensorOrder idx_0 = 0; idx_0 < width; ++idx_0) {
-      for (TensorOrder idx_1 = 0; idx_1 < width; ++idx_1) {
-        TensorOrder org_offset = (idx_0 + offset_org_0) * this->data_width
+    for (TensorIdx idx_0 = 0; idx_0 < width; ++idx_0) {
+      for (TensorIdx idx_1 = 0; idx_1 < width; ++idx_1) {
+        TensorIdx org_offset = (idx_0 + offset_org_0) * this->data_width
             + idx_1 + offset_org_1;
-        TensorOrder ref_offset = idx_0 + offset_ref_0
+        TensorIdx ref_offset = idx_0 + offset_ref_0
             + (idx_1 + offset_ref_1) * this->data_width;
 
         if (CoefUsage::USE_NONE == USAGE)
@@ -101,54 +101,52 @@ protected:
     copy(this->org_data, this->org_data + this->data_len, this->act_data);
   }
 
-  // Cannot use multiple template parameter in test cases
-  template <CoefUsage USAGE>
-  TensorIdx test_offset_full(TensorOrder offset_org_0, TensorOrder offset_org_1,
-      TensorOrder offset_out_0, TensorOrder offset_out_1) {
-    // Prepare
-    // Create kernel, compute reference and reset actual data
-    KernelTransAvx<FloatType, USAGE, KernelType::KERNEL_FULL> kernel;
-    this->reg_alpha = kernel.reg_coef(this->alpha);
-    this->reg_beta = kernel.reg_coef(this->beta);
-    this->calc_ref<USAGE>(offset_org_0, offset_org_1, offset_out_0,
-        offset_out_1, this->kernel_width_full);
-    this->reset_act();
+  template <CoefUsage USAGE,
+            KernelType TYPE>
+  class CaseGenerator {
+  public:
+    CaseGenerator(TestKernelTransAvx<FloatType> &outer)
+        : outer(outer) {
+    }
 
-    // Execute transpose
-    kernel(this->org_data + offset_org_0 * this->data_width + offset_org_1,
-        this->act_data + offset_out_0 + offset_out_1 * this->data_width,
-        this->data_width, this->data_width, this->reg_alpha, this->reg_beta);
+    array<TensorIdx, 5> operator()() {
+      // Create kernel, compute reference and reset actual data
+      KernelTransAvx<FloatType, USAGE, TYPE> kernel;
+      outer.reg_alpha = kernel.reg_coef(outer.alpha);
+      outer.reg_beta = kernel.reg_coef(outer.beta);
+      array<TensorIdx, 5> result{ -1, 0, 0, 0, 0 };
 
-    // Verify
-    return verify<FloatType>(this->ref_data, this->act_data, this->data_len);
-  }
+      // Execute transpose
+      for (TensorIdx &org_0 = result[1]; org_0 < outer.extra; ++org_0)
+        for (TensorIdx &org_1 = result[2]; org_1 < outer.extra; ++org_1)
+          for (TensorIdx &act_0 = result[3]; act_0 < outer.extra; ++act_0)
+            for (TensorIdx &act_1 = result[4]; act_1 < outer.extra; ++act_1) {
+              outer.calc_ref<USAGE>(org_0, org_1, act_0, act_1,
+                  kernel.get_reg_num());
+              outer.reset_act();
+              kernel(outer.org_data + org_0 * outer.data_width + org_1,
+                  outer.act_data + act_0 + act_1 * outer.data_width,
+                  outer.data_width, outer.data_width, outer.reg_alpha,
+                  outer.reg_beta);
+              result[0] = verify<FloatType>(outer.ref_data, outer.act_data,
+                  outer.data_len);
+              if (-1 != result[0])
+                return result;
+            }
 
-  template <CoefUsage USAGE>
-  TensorIdx test_offset_half(TensorOrder offset_org_0, TensorOrder offset_org_1,
-      TensorOrder offset_out_0, TensorOrder offset_out_1) {
-    // Prepare
-    // Create kernel, compute reference and reset actual data
-    KernelTransAvx<FloatType, USAGE, KernelType::KERNEL_HALF> kernel;
-    this->reg_alpha = kernel.reg_coef(this->alpha);
-    this->reg_beta = kernel.reg_coef(this->beta);
-    this->calc_ref<USAGE>(offset_org_0, offset_org_1, offset_out_0,
-        offset_out_1, this->kernel_width_half);
-    this->reset_act();
+      return result;
+    }
 
-    // Execute transpose
-    kernel(this->org_data + offset_org_0 * this->data_width + offset_org_1,
-        this->act_data + offset_out_0 + offset_out_1 * this->data_width,
-        this->data_width, this->data_width, this->reg_alpha, this->reg_beta);
-
-    // Verify
-    return verify<FloatType>(this->ref_data, this->act_data, this->data_len);
-  }
+  private:
+    TestKernelTransAvx<FloatType> &outer;
+  };
 
 
   constexpr static TensorOrder extra = 2;
   constexpr static TensorOrder in_offset = sizeof(FloatType) / sizeof(Deduced);
   constexpr static Deduced alpha = static_cast<Deduced>(2.3f);
   constexpr static Deduced beta = static_cast<Deduced>(4.2f);
+
   GenNumType kernel_width_full, kernel_width_half;
   TensorIdx data_width, data_len;
   FloatType *org_data, *ref_data, *act_data;
@@ -175,132 +173,94 @@ TYPED_TEST(TestKernelTransAvx, TestUtilities) {
 
 
 TYPED_TEST(TestKernelTransAvx, TestFullCoefNone) {
-  for (TensorOrder org_0 = 0; org_0 < this->extra; ++org_0)
-    for (TensorOrder org_1 = 0; org_1 < this->extra; ++org_1)
-      for (TensorOrder act_0 = 0; act_0 < this->extra; ++act_0)
-        for (TensorOrder act_1 = 0; act_1 < this->extra; ++act_1) {
-          auto result = this->test_offset_full<CoefUsage::USE_NONE>(
-              org_0, org_1, act_0, act_1);
-          ASSERT_EQ(-1, result) << "Result of full kernel transpose without"
-              << " coefficient does not match at absolute index: " << result
-              << ", offsets are: Original: (" << org_0 << ", " << org_1
-              << "), Actual: (" << act_0 << ", " << act_1 <<").";
-        }
+  typename TestKernelTransAvx<TypeParam>::CaseGenerator<CoefUsage::USE_NONE,
+      KernelType::KERNEL_FULL> test_full(*this);
+  auto result = test_full();
+  ASSERT_EQ(-1, result[0]) << "Result of full kernel transpose without"
+      << " coefficient does not match at absolute index: " << result[0]
+      << ", offsets are: Original: (" << result[1] << ", " << result[2]
+      << "), Actual: (" << result[3] << ", " << result[4] << ").";
 }
 
 
 TYPED_TEST(TestKernelTransAvx, TestFullCoefAlpha) {
-  for (TensorOrder org_0 = 0; org_0 < this->extra; ++org_0)
-    for (TensorOrder org_1 = 0; org_1 < this->extra; ++org_1)
-      for (TensorOrder act_0 = 0; act_0 < this->extra; ++act_0)
-        for (TensorOrder act_1 = 0; act_1 < this->extra; ++act_1) {
-          auto result = this->test_offset_full<CoefUsage::USE_ALPHA>(
-              org_0, org_1, act_0, act_1);
-          ASSERT_EQ(-1, result) << "Result of full kernel transpose with alpha"
-              << " does not match at absolute index: " << result
-              << ", offsets are: Original: (" << org_0 << ", " << org_1
-              << "), Actual: (" << act_0 << ", " << act_1 <<").";
-        }
+  typename TestKernelTransAvx<TypeParam>::CaseGenerator<CoefUsage::USE_ALPHA,
+      KernelType::KERNEL_FULL> test_full(*this);
+  auto result = test_full();
+  ASSERT_EQ(-1, result[0]) << "Result of full kernel transpose with alpha"
+      << " does not match at absolute index: " << result[0]
+      << ", offsets are: Original: (" << result[1] << ", " << result[2]
+      << "), Actual: (" << result[3] << ", " << result[4] << ").";
 }
 
 
 TYPED_TEST(TestKernelTransAvx, TestFullCoefBeta) {
-  for (TensorOrder org_0 = 0; org_0 < this->extra; ++org_0)
-    for (TensorOrder org_1 = 0; org_1 < this->extra; ++org_1)
-      for (TensorOrder act_0 = 0; act_0 < this->extra; ++act_0)
-        for (TensorOrder act_1 = 0; act_1 < this->extra; ++act_1) {
-          auto result = this->test_offset_full<CoefUsage::USE_BETA>(
-              org_0, org_1, act_0, act_1);
-          ASSERT_EQ(-1, result) << "Result of full kernel transpose with beta"
-              << " does not match at absolute index: " << result
-              << ", offsets are: Original: (" << org_0 << ", " << org_1
-              << "), Actual: (" << act_0 << ", " << act_1 <<").";
-        }
+  typename TestKernelTransAvx<TypeParam>::CaseGenerator<CoefUsage::USE_BETA,
+      KernelType::KERNEL_FULL> test_full(*this);
+  auto result = test_full();
+  ASSERT_EQ(-1, result[0]) << "Result of full kernel transpose with beta"
+      << " does not match at absolute index: " << result[0]
+      << ", offsets are: Original: (" << result[1] << ", " << result[2]
+      << "), Actual: (" << result[3] << ", " << result[4] << ").";
 }
 
 
 TYPED_TEST(TestKernelTransAvx, TestFullCoefBoth) {
-  for (TensorOrder org_0 = 0; org_0 < this->extra; ++org_0)
-    for (TensorOrder org_1 = 0; org_1 < this->extra; ++org_1)
-      for (TensorOrder act_0 = 0; act_0 < this->extra; ++act_0)
-        for (TensorOrder act_1 = 0; act_1 < this->extra; ++act_1) {
-          auto result = this->test_offset_full<CoefUsage::USE_BOTH>(
-              org_0, org_1, act_0, act_1);
-          ASSERT_EQ(-1, result) << "Result of full kernel transpose with both"
-              << " coefficients does not match at absolute index: " << result
-              << ", offsets are: Original: (" << org_0 << ", " << org_1
-              << "), Actual: (" << act_0 << ", " << act_1 <<").";
-        }
+  typename TestKernelTransAvx<TypeParam>::CaseGenerator<CoefUsage::USE_BOTH,
+      KernelType::KERNEL_FULL> test_full(*this);
+  auto result = test_full();
+  ASSERT_EQ(-1, result[0]) << "Result of full kernel transpose with both"
+      << " coefficients does not match at absolute index: " << result[0]
+      << ", offsets are: Original: (" << result[1] << ", " << result[2]
+      << "), Actual: (" << result[3] << ", " << result[4] << ").";
 }
 
 
-/*
- * Following tests always pass before half kernels are implemented.
- */
-
 TYPED_TEST(TestKernelTransAvx, TestHalfCoefNone) {
-  for (TensorOrder org_0 = 0; org_0 < this->extra; ++org_0)
-    for (TensorOrder org_1 = 0; org_1 < this->extra; ++org_1)
-      for (TensorOrder act_0 = 0; act_0 < this->extra; ++act_0)
-        for (TensorOrder act_1 = 0; act_1 < this->extra; ++act_1) {
-          /*auto result = this->test_offset_half<CoefUsage::USE_NONE>(
-              org_0, org_1, act_0, act_1);*/
-          auto result = -1;
-          ASSERT_EQ(-1, result) << "Result of half kernel transpose without"
-              << " coefficient does not match at absolute index: " << result
-              << ", offsets are: Original: (" << org_0 << ", " << org_1
-              << "), Actual: (" << act_0 << ", " << act_1 <<").";
-        }
+  typename TestKernelTransAvx<TypeParam>::CaseGenerator<CoefUsage::USE_NONE,
+      KernelType::KERNEL_HALF> test_half(*this);
+  // auto result = test_half();
+  array<TensorIdx, 5> result{ -1, 0, 0, 0, 0 };
+  ASSERT_EQ(-1, result[0]) << "Result of half kernel transpose without"
+      << " coefficient does not match at absolute index: " << result[0]
+      << ", offsets are: Original: (" << result[1] << ", " << result[2]
+      << "), Actual: (" << result[3] << ", " << result[4] << ").";
 }
 
 
 TYPED_TEST(TestKernelTransAvx, TestHalfCoefAlpha) {
-  for (TensorOrder org_0 = 0; org_0 < this->extra; ++org_0)
-    for (TensorOrder org_1 = 0; org_1 < this->extra; ++org_1)
-      for (TensorOrder act_0 = 0; act_0 < this->extra; ++act_0)
-        for (TensorOrder act_1 = 0; act_1 < this->extra; ++act_1) {
-          /*auto result = this->test_offset_half<CoefUsage::USE_ALPHA>(
-              org_0, org_1, act_0, act_1);*/
-          auto result = -1;
-          ASSERT_EQ(-1, result) << "Result of half kernel transpose with alpha"
-              << " does not match at absolute index: " << result
-              << ", offsets are: Original: (" << org_0 << ", " << org_1
-              << "), Actual: (" << act_0 << ", " << act_1 <<").";
-        }
+  typename TestKernelTransAvx<TypeParam>::CaseGenerator<CoefUsage::USE_ALPHA,
+      KernelType::KERNEL_HALF> test_half(*this);
+  // auto result = test_half();
+  array<TensorIdx, 5> result{ -1, 0, 0, 0, 0 };
+  ASSERT_EQ(-1, result[0]) << "Result of half kernel transpose with alpha"
+      << " does not match at absolute index: " << result[0]
+      << ", offsets are: Original: (" << result[1] << ", " << result[2]
+      << "), Actual: (" << result[3] << ", " << result[4] << ").";
 }
 
 
 TYPED_TEST(TestKernelTransAvx, TestHalfCoefBeta) {
-  for (TensorOrder org_0 = 0; org_0 < this->extra; ++org_0)
-    for (TensorOrder org_1 = 0; org_1 < this->extra; ++org_1)
-      for (TensorOrder act_0 = 0; act_0 < this->extra; ++act_0)
-        for (TensorOrder act_1 = 0; act_1 < this->extra; ++act_1) {
-          /*auto result = this->test_offset_half<CoefUsage::USE_BETA>(
-              org_0, org_1, act_0, act_1);*/
-          auto result = -1;
-          ASSERT_EQ(-1, result) << "Result of half kernel transpose with beta"
-              << " does not match at absolute index: " << result
-              << ", offsets are: Original: (" << org_0 << ", " << org_1
-              << "), Actual: (" << act_0 << ", " << act_1 <<").";
-        }
+  typename TestKernelTransAvx<TypeParam>::CaseGenerator<CoefUsage::USE_BETA,
+      KernelType::KERNEL_HALF> test_half(*this);
+  // auto result = test_half();
+  array<TensorIdx, 5> result{ -1, 0, 0, 0, 0 };
+  ASSERT_EQ(-1, result[0]) << "Result of half kernel transpose with beta"
+      << " does not match at absolute index: " << result[0]
+      << ", offsets are: Original: (" << result[1] << ", " << result[2]
+      << "), Actual: (" << result[3] << ", " << result[4] << ").";
 }
 
 
 TYPED_TEST(TestKernelTransAvx, TestHalfCoefBoth) {
-  for (TensorOrder org_0 = 0; org_0 < this->extra; ++org_0)
-    for (TensorOrder org_1 = 0; org_1 < this->extra; ++org_1)
-      for (TensorOrder act_0 = 0; act_0 < this->extra; ++act_0)
-        for (TensorOrder act_1 = 0; act_1 < this->extra; ++act_1) {
-          /*auto result = this->test_offset_half<CoefUsage::USE_BOTH>(
-              org_0, org_1, act_0, act_1);*/
-          auto result = -1;
-          ASSERT_EQ(-1, result) << "Result of half kernel transpose with both"
-              << " coefficients does not match at absolute index: " << result
-              << ", offsets are: Original: (" << org_0 << ", " << org_1
-              << "), Actual: (" << act_0 << ", " << act_1 <<").";
-        }
+  typename TestKernelTransAvx<TypeParam>::CaseGenerator<CoefUsage::USE_BOTH,
+      KernelType::KERNEL_HALF> test_half(*this);
+  // auto result = test_half();
+  array<TensorIdx, 5> result{ -1, 0, 0, 0, 0 };
+  ASSERT_EQ(-1, result[0]) << "Result of half kernel transpose with both"
+      << " coefficients does not match at absolute index: " << result[0]
+      << ", offsets are: Original: (" << result[1] << ", " << result[2]
+      << "), Actual: (" << result[3] << ", " << result[4] << ").";
 }
-
-
 
 #endif // HPTC_UNIT_TEST_KERNELS_TEST_KERNEL_TRANS_AVX_H_
