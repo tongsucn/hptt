7ba1d83594e12abae36c92e7a8a6eb94d053772c
Paul Springer <springer@aices.rwth-aachen.de>
2017-03-23 10:15:02 +0100

    minor changes to the comments

--- inc/hptc/hptc_trans.h
+++ inc/hptc/hptc_trans.h
@@ -27,10 +27,10 @@ using OuterSize = std::pair<std::vector<TensorOrder>,
 /**
  * \brief Function for creating a tensor transpose.
  *
- * \details This function is used to create computational graph for transposing
+ * \details This function is used to create computational a graph for transposing
  *     a tensor. For the flexibility, the transpose has following form:
  *
- * $B = \alpha \cdot A + \beta \cdot B$
+ * $B_{perm(i1,i2,..., iN)} = \alpha \cdot A_{i_1,i_2,..., i_N} + \beta \cdot B_{perm(i_1,i_2,..., i_N)}$
  *
  * where A is an input tensor, B is the output destination. There are three
  * template parameters serving for this function: The transpose strategy is
@@ -51,6 +51,8 @@ using OuterSize = std::pair<std::vector<TensorOrder>,
  *     is set to CoefUsageTrans::USE_BETA or CoefUsageTrans::USE_ALPHA.
  * \param[in] in_size Vector used for describing size of each order in input
  *     tensor.
+ * \param[in] perm Vector used for describing permutation (e.g., perm={1,0}
+ *     denotes a matrix transpose).
  * \param[in] alpha Coefficient for scaling all the elements in input tensor.
  * \param[in] beta Coefficient for updating output tensor.
  * \param[in] thread_num Thread number for transpose, unsigned integer. When
@@ -61,17 +63,17 @@ using OuterSize = std::pair<std::vector<TensorOrder>,
  * \param[in] out_outer_size An std::pair for describing output tensor outer
  *     size, First is the outer size vector, second is offset for each order.
  *     Optional parameter, default's first is an empty std::initializer_list;
- * \param[in] tune_num Number of tuning. Optional parameter, the default value
- *       is 0. Its input value could be:
+ * \param[in] tune_num Number of candidate implementations that should be
+ *       evaluated; this is an optional parameter (default value
+ *       is 0). Its input value could be:
  *     - tune_num < 0: Tune all generated transpose candidate. Caution: although
  *       tuning all possible results could deliver best transpose performance,
  *       it will cost plenty of time before getting the solution as well,
- *       especially when ORDER is large. The tune number increases in factorial
- *       speed ($n!$).
- *     - tune_num == 0: No tuning will be performed, directly pick up the
- *       strategy from static rule or heuristics.
+ *       especially when ORDER $n$ is large. The number of candidates increases with $n!$.
+ *     - tune_num == 0: No tuning will be performed and a suitable candidate
+ *     according to the performance model will be selected automatically.
  *     - tune_num > 0: tune_num candidates will be measured and compared.
- * \param[in] tune_num Number of heuristic generation. Optional parameter,
+ * \param[in] heur_num Number of heuristic generation. Optional parameter,
  *     default value is 0. HPTC will generated sqrt(heur_num) loop order
  *     candidates and sqrt(heur_num) parallelization strategy candidates. Those
  *     candidates will be evaluated and sorted. The top candidates (depends on
@@ -87,8 +89,6 @@ using OuterSize = std::pair<std::vector<TensorOrder>,
  * \code{.cpp}
  *   auto graph = create_cgraph_trans(...);  // Create graph
  *   if (nullptr != graph) {
- *     (*graph)();      // Use overloaded operator()
- *                      // or ...
  *     graph->exec();   // use cgraph's member function exec()
  *   }
  * \endcode
