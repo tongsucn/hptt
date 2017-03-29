3df12ca80fb193c1d6ff9725e433aa16b95330e1
Tong Su <tongsu.cn@gmail.com>
2017-03-29 18:00:42 +0200

    Update libraries dependencies to avoid benchmark fail in some cases.

--- cmake/hptc_install.cmake
+++ cmake/hptc_install.cmake
@@ -14,23 +14,3 @@ macro(hptc_set_header_install)
   message("-- Header files will be installed in "
     ${CMAKE_INSTALL_PREFIX}/include/hptc)
 endmacro()
-
-macro(hptc_set_lib_install LIB_NAME)
-  # Shared/static library
-  set(HPTC_LIB_INSTALL_TARGET_DIR lib)
-  install(TARGETS ${LIB_NAME}
-    LIBRARY DESTINATION ${HPTC_LIB_INSTALL_TARGET_DIR})
-
-  message("-- Shared library lib" ${LIB_NAME} ".so will be installed in "
-    ${CMAKE_INSTALL_PREFIX}/${HPTC_LIB_INSTALL_TARGET_DIR})
-endmacro()
-
-macro(hptc_set_lib_static_install LIB_NAME)
-  # Shared/static library
-  set(HPTC_LIB_INSTALL_TARGET_DIR lib)
-  install(TARGETS ${LIB_NAME}
-    ARCHIVE DESTINATION ${HPTC_LIB_INSTALL_TARGET_DIR})
-
-  message("-- Static library lib" ${LIB_NAME} ".so will be installed in "
-    ${CMAKE_INSTALL_PREFIX}/${HPTC_LIB_INSTALL_TARGET_DIR})
-endmacro()
