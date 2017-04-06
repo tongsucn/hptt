b85888013d7635a976ed0bc2d6e14372e0b0511a
Tong Su <tongsu.cn@gmail.com>
2017-04-06 21:31:18 +0200

    Added a new single threaded benchmark.

---
+++ test/benchmark/trans/avx/benchmark_s_0132_24x16x24x16.cc
@@ -0,0 +1,15 @@
+#include <hptc/benchmark/benchmark_trans.h>
+#include <hptc/perf-test/test_perf_util.h>
+#include <hptc/test_util.h>
+
+using namespace hptc;
+
+
+int main() {
+  RefTrans<float> ref_trans;
+  auto &ref_config = ref_trans_configs[57];
+
+  compare_perf<float, RefTrans<float>, 4>(ref_trans, ref_config);
+
+  return 0;
+}
